f8117c33fb43b03dd92a27c8c62602b70499f9e6
Ahmad Ghulam Zakiy <ghulam.zakiy@gmail.com>
2021-04-09 18:25:30 +0800

    Fix error handling caused by bad params and route

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from falcon import API
+from falcon import API, HTTPBadRequest, HTTPNotFound
 from database import Wilayah_Indonesia
 from pony.orm import db_session, raw_sql
 
@@ -6,67 +6,76 @@ from pony.orm import db_session, raw_sql
 app = application = API()
 # implemented in Falcon Framework
 
+
+def validCode(kode, length=2):
+    return kode and kode.isdigit() and len(kode) == length
+
+
 class Indonesia():
     @db_session
     def on_get(self, req, resp):
-        # print('indo')
         kode = "__"
         wil = Wilayah_Indonesia.select(lambda w: raw_sql('kode like "' + kode + '"'))
         if len(wil) > 0:
             resp.media = {
                 "jumlah": len(wil),
-                "data": [{"id": w.kode, "nama": w.nama} for w in wil]
+                "data": [{"id": w.kode, "nama": w.nama, "tipe": "provinsi"} for w in wil]
             }
+        else:
+            raise HTTPNotFound()
 
 
 class Provinsi():
     @db_session
     def on_get(self, req, resp, prov, **params):
-        # print('prov ', prov)
-        if prov and len(prov) == 2:
-            kode = prov + '___'
+        if not validCode(prov):
+            raise HTTPBadRequest()
+
+        kode = prov + '___'
         wil = Wilayah_Indonesia.select(lambda w: raw_sql('kode like "' + kode + '"'))
         if len(wil) > 0:
             resp.media = {
                 "jumlah": len(wil),
-                "data": [{"id": w.kode, "nama": w.nama} for w in wil]
+                "data": [{"id": w.kode, "nama": w.nama, "tipe": "kabupaten"} for w in wil]
             }
+        else:
+            raise HTTPNotFound()
 
 
 class Kabupaten():
     @db_session
     def on_get(self, req, resp, prov, kab, **params):
-        # print('prov ', prov)
-        # print('kab ', kab)
         kode = ""
-        if prov and len(prov) == 2:
-            if kab and len(kab) == 2:
-                kode = prov + '.' + kab + '___'
+        if not validCode(prov) or not validCode(kab):
+            raise HTTPBadRequest()
+
+        kode = prov + '.' + kab + '___'
         wil = Wilayah_Indonesia.select(lambda w: raw_sql('kode like "' + kode + '"'))
         if len(wil) > 0:
             resp.media = {
                 "jumlah": len(wil),
-                "data": [{"id": w.kode, "nama": w.nama} for w in wil]
+                "data": [{"id": w.kode, "nama": w.nama, "tipe": "kecamatan"} for w in wil]
             }
+        else:
+            raise HTTPNotFound()
 
 
 class Kecamatan():
     @db_session
     def on_get(self, req, resp, prov, kab, kec, **params):
-        # print('prov ', prov)
-        # print('kab ', kab)
-        # print('kec ', kec)
         kode = "__"
-        if prov and len(prov) == 2:
-            if kab and len(kab) == 2:
-                if kec and len(kec) == 2:
-                    kode = prov + '.' + kab + '.' + kec + '_____'
+        if not validCode(prov) or not validCode(kab) or not validCode(kec):
+            raise HTTPBadRequest()
+
+        kode = prov + '.' + kab + '.' + kec + '_____'
         wil = Wilayah_Indonesia.select(lambda w: raw_sql('kode like "' + kode + '"'))
         if len(wil) > 0:
             resp.media = {
                 "jumlah": len(wil),
-                "data": [{"id": w.kode, "nama": w.nama} for w in wil]
+                "data": [{"id": w.kode, "nama": w.nama, "tipe": "kelurahan"} for w in wil]
             }
+        else:
+            raise HTTPNotFound()
 
 
 app.add_route('/indonesia', Indonesia())
