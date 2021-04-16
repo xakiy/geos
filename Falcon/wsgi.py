a645890fe16703fc37ff7f753ed1e34da52037e5
Ahmad Ghulam Zakiy <ghulam.zakiy@gmail.com>
2021-04-16 15:16:16 +0800

    Refactor wsgi.py and create independent regions.py file

---
+++ Falcon/wsgi.py
@@ -0,0 +1,73 @@
+from falcon import API, HTTPBadRequest, HTTPNotFound
+import regions
+
+
+# implemented in Falcon Framework
+app = application = API()
+
+# strip any trailing slash in request params
+app.req_options.strip_url_path_trailing_slash = True
+
+
+class Indonesia():
+    def on_get(self, req, resp):
+        data = regions.Indonesia.get_provinces()
+        if data:
+            resp.media = data
+        else:
+            raise HTTPNotFound()
+
+
+class Provinsi():
+    def on_get(self, req, resp, prov, **params):
+        try:
+            data = regions.Indonesia.get_regencies(prov)
+            if data:
+                resp.media = data
+            else:
+                raise HTTPNotFound()
+        except TypeError:
+            raise HTTPBadRequest()
+
+
+class Kabupaten():
+    def on_get(self, req, resp, prov, kab, **params):
+        try:
+            data = regions.Indonesia.get_districts(prov, kab)
+            if data:
+                resp.media = data
+            else:
+                raise HTTPNotFound()
+        except TypeError:
+            raise HTTPBadRequest()
+
+
+class Kecamatan():
+    def on_get(self, req, resp, prov, kab, kec, **params):
+        try:
+            data = regions.Indonesia.get_localities(prov, kab, kec)
+            if data:
+                resp.media = data
+            else:
+                raise HTTPNotFound()
+        except TypeError:
+            raise HTTPBadRequest()
+
+
+class Kelurahan():
+    def on_get(self, req, resp, prov, kab, kec, kel, **params):
+        try:
+            data = regions.Indonesia.get_town(prov, kab, kec, kel)
+            if data:
+                resp.media = data
+            else:
+                raise HTTPNotFound()
+        except TypeError:
+            raise HTTPBadRequest()
+
+
+app.add_route('/indonesia', Indonesia())
+app.add_route('/indonesia/{prov}', Provinsi())
+app.add_route('/indonesia/{prov}/{kab}', Kabupaten())
+app.add_route('/indonesia/{prov}/{kab}/{kec}', Kecamatan())
+app.add_route('/indonesia/{prov}/{kab}/{kec}/{kel}', Kelurahan())
---
+++ regions.py
@@ -0,0 +1,83 @@
+from database import Wilayah_2020 as Wilayah_Indonesia
+from pony.orm import db_session, raw_sql
+
+
+def validateCode(kode, length=2):
+    if not (kode and kode.isdigit() and len(kode) == length):
+        raise TypeError('Invalid Params: "{}"'.format(kode))
+
+
+class Indonesia():
+    @staticmethod
+    @db_session
+    def get_provinces():
+        wil = Wilayah_Indonesia.select(lambda w: raw_sql('kode like "__"'))
+        if len(wil) > 0:
+            return {
+                "jumlah": len(wil),
+                "data": [{"id": w.kode, "nama": w.nama.title(), "tipe": "provinsi"} for w in wil]
+            }
+        else:
+            return False
+
+    @staticmethod
+    @db_session
+    def get_regencies(prov):
+        validateCode(prov)
+        kode = prov + '___'
+        wil = Wilayah_Indonesia.select(lambda w: raw_sql('kode like "' + kode + '"'))
+        if len(wil) > 0:
+            return {
+                "jumlah": len(wil),
+                "data": [{"id": w.kode, "nama": w.nama.title(), "tipe": "kabupaten"} for w in wil]
+            }
+        else:
+            return False
+
+    @staticmethod
+    @db_session
+    def get_districts(prov, kab):
+        validateCode(prov)
+        validateCode(kab)
+        kode = prov + '.' + kab + '___'
+        wil = Wilayah_Indonesia.select(lambda w: raw_sql('kode like "' + kode + '"'))
+        if len(wil) > 0:
+            return {
+                "jumlah": len(wil),
+                "data": [{"id": w.kode, "nama": w.nama.title(), "tipe": "kecamatan"} for w in wil]
+            }
+        else:
+            return False
+
+    @staticmethod
+    @db_session
+    def get_localities(prov, kab, kec):
+        validateCode(prov)
+        validateCode(kab)
+        validateCode(kec)
+        kode = prov + '.' + kab + '.' + kec + '_____'
+        wil = Wilayah_Indonesia.select(lambda w: raw_sql('kode like "' + kode + '"'))
+        if len(wil) > 0:
+            return {
+                "jumlah": len(wil),
+                "data": [{"id": w.kode, "nama": w.nama.title(), "tipe": "kelurahan"} for w in wil]
+            }
+        else:
+            return False
+
+    @staticmethod
+    @db_session
+    def get_town(prov, kab, kec, kel):
+        validateCode(prov)
+        validateCode(kab)
+        validateCode(kec)
+        validateCode(kel, 4)
+        kode = prov + '.' + kab + '.' + kec + '.' + kel
+        wil = Wilayah_Indonesia.select(lambda w: raw_sql('kode like "' + kode + '"'))
+        if len(wil) > 0:
+            return {
+                "jumlah": len(wil),
+                "data": [{"id": w.kode, "nama": w.nama.title(), "tipe": "kelurahan"} for w in wil]
+            }
+        else:
+            return False
