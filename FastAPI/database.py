006a309642abe9f5ebba8cf54beb0a61bd542d55
Ahmad Ghulam Zakiy <ghulam.zakiy@gmail.com>
2021-04-16 17:36:20 +0800

    Docker build failed due to unable to copy symlink files
    Change types from symlink to hardlink

--- FastAPI/database.py
+++ FastAPI/database.py
@@ -1,1 +1,14 @@
-../database.py
+from pony.orm import Database, PrimaryKey, Optional
+from pathlib import Path
+
+
+db = Database()
+
+
+class Wilayah_2020(db.Entity):
+    kode = PrimaryKey(str)
+    nama = Optional(str)
+
+db_file = Path(__file__).absolute().parent / 'geo.db'
+db.bind("sqlite", filename=str(db_file.absolute()), create_db=False)
+db.generate_mapping(create_tables=False)
