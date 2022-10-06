5331819fbca49e3006de390d61ba8233a10b5b88
Ahmad Ghulam Zakiy <ghulam.zakiy@gmail.com>
2022-10-07 00:05:42 +0800

    Karena file wilayah_2022.sql atau wilayah.sql sudah diubah formatnya menjadi universal SQL dump, maka konversi menggunakan mysql2sqlite tidak diperlukan lagi. SQL dump terbaru sudah bisa langsung di pipe ke SQLite3 untuk di ubah menjadi database.

--- rebuild-db.py
+++ rebuild-db.py
@@ -3,10 +3,22 @@ import os
 import time
 import subprocess
 
-database_raw_url = 'https://raw.github.com/cahyadsn/wilayah/master/db/wilayah_2020.sql'
+
+"""
+Note:
+Sejak update database wilayah_2022.sql, format dump SQL yang dipakai telah diubah tidak
+lagi spesifik MySQL format dump, tetapi sudah menjadi format universal SQL dump.
+Oleh karena itu, SQLite sudah bisa langsung menggunakan format dump tersebut dan
+sudah tidak membutuhkan konverter mysql2sqlite lagi.
+
+Tetapi untuk dokumentasi, saya akan tetap menyertakan kode konverternya.
+"""
+
+# database_raw_url = 'https://raw.github.com/cahyadsn/wilayah/master/db/wilayah_2020.sql'
+database_raw_url = 'https://raw.github.com/cahyadsn/wilayah/master/db/wilayah.sql'
 input_db = os.path.basename(database_raw_url)
 output_db = 'geo.db'
-mysql2sqlite = './bin/mysql2sqlite'
+# mysql2sqlite = './bin/mysql2sqlite'
 sqlite = 'sqlite3'
 ready = True
 
@@ -23,12 +35,12 @@ else:
 
 # Checking required files
 print('Memeriksa berkas-berkas yang dibutuhkan:')
-print(' - {}\t\t '.format(os.path.basename(mysql2sqlite)), end='')
-if os.path.isfile(mysql2sqlite):
-    print('ada')
-else:
-    print('tidak')
-    ready = False
+# print(' - {}\t\t '.format(os.path.basename(mysql2sqlite)), end='')
+# if os.path.isfile(mysql2sqlite):
+#     print('ada')
+# else:
+#     print('tidak')
+#     ready = False
 
 print(' - {}\t\t '.format(input_db), end='')
 if os.path.isfile(input_db):
@@ -55,7 +67,8 @@ except Exception:
 
 print('Memkonversi database ke sqlite format ... ', end='')
 try:
-    subprocess.run('{} {} | {} {}'.format(mysql2sqlite, input_db, sqlite, output_db), shell=True)
+    # subprocess.run('{} {} | {} {}'.format(mysql2sqlite, input_db, sqlite, output_db), shell=True)
+    subprocess.run('cat {} | {} {}'.format(input_db, sqlite, output_db), shell=True)
 except Exception:
     print('gagal!')
 else:
@@ -63,3 +76,18 @@ else:
         print('berhasil!')
     else:
         print('gagal!')
+
+# Fungsi untuk menambahkan backtick di dump SQL
+def quote_field(input_file, output_file):
+    i = open(input_file, 'r')
+    o = open(output_file,'w')
+    line = i.readline()
+    while line:
+        line = re.sub(" wilayah",' "wilayah"', line)
+        line = re.sub(" kode ",' "kode" ', line)
+        line = re.sub(" nama ",' "nama" ', line)
+        line = re.sub("kode, nama", '"kode", "nama"', line)
+        o.write(line)
+        line = i.readline()
+    i.close()
+    o.close()
